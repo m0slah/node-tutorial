d9a736aae07d81541a482e5572788d513158f51d
m0slah <muhammedslahqadir@gmail.com>
2024-09-09 17:52:52 +0300

    create Module file

---
+++ 01-intro.js
@@ -0,0 +1,9 @@
+const amount = 9;
+
+if (amount < 10) {
+  console.log("Less than 10");
+} else {
+  console.log("more than 10");
+}
+
+console.log("first node program");
--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-const amount = 9;
-
-if (amount < 10) {
-  console.log("Less than 10");
-} else {
-  console.log("more than 10");
-}
-
-console.log("first node program");
+//Modules
